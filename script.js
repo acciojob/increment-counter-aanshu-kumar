42b72d41e5013d89aa8b6dea114ce760d1cd9d29
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-20 15:46:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function increment()
+{
+    let counter = document.getElementById("counter");
+    counter.innerHTML=Number(counter.textContent)+1;
+	alert(counter.textContent);
+}
